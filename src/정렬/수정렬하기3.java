1f35f6f86c73e018285ab4c21e08d2d28c2fa9f2
meeyoungchoi <42866800+meeyoungchoi@users.noreply.github.com>
2022-02-23 21:07:10 +0900

    02월 23일 정렬 관련 알고리즘 문제풀이 및 회고

---
+++ src/정렬/수정렬하기3.java
@@ -0,0 +1,23 @@
+package 정렬;
+import java.io.*;
+import java.util.*;
+
+public class 수정렬하기3 {
+    public static void main(String[] args) throws Exception, IOException {
+
+        int[] arr = new int[10000];
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(bf.readLine());
+        for( int i = 0 ; i < num ; i++) {
+            int a = Integer.parseInt(bf.readLine());
+            arr[a-1]++;
+        }
+        for( int i = 0 ; i < 10000 ; i++) {
+            for( int j = 0 ; j < arr[i] ; j++) {
+                bw.write(i+1+"\n");
+            }
+        }
+        bw.flush();
+    }
+}
