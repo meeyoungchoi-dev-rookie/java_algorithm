3c39ca2b45ff3ab77f079b4ce94953edc12d6107
meeyoungchoi <42866800+meeyoungchoi@users.noreply.github.com>
2022-03-03 20:49:32 +0900

    03월 03일 문자열 아스키코드 문제풀이 및 회고

---
+++ src/문자열/아스키코드.java
@@ -0,0 +1,17 @@
+package 문자열;
+
+import java.util.Scanner;
+
+public class 아스키코드 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int ch = scanner.next().charAt(0);
+        System.out.println(ch);
+
+
+
+
+    }
+}
