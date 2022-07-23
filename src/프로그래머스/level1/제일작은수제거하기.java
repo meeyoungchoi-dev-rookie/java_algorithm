0de044ce10ca19ef9c31be0e372d5a16f62d23ba
meeyoungchoi <goodlife1359@gmail.com>
2022-07-23 15:40:07 +0900

    프로그래머스 Level1 정수 제일 작은수 제거하기 코드 리펙토링

--- src/프로그래머스/level1/제일작은수제거하기.java
+++ src/프로그래머스/level1/제일작은수제거하기.java
@@ -1,8 +1,5 @@
 package 프로그래머스.level1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class 제일작은수제거하기 {
     public static void main(String[] args) {
         int[] arr = {4, 3, 2, 1};
@@ -12,29 +9,26 @@ public class 제일작은수제거하기 {
 
 
     public static int[] solution(int[] arr) {
-        int[] temp = arr.clone();
-        Arrays.sort(temp);
-        int min = temp[0];
-        //ArrayList를 이용하여 최솟값과 같지 않은 수만 추가
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (arr.length == 1) {
+            int[] answer = {-1};
+            return answer;
+        }
+
+        int[] answer = new int[arr.length - 1];
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+        }
+
+        int index = 0;
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] != min) {
-                list.add(arr[i]);
+            if (arr[i] == min) {
+                continue;
             }
+            answer[index] = arr[i];
+            index += 1;
         }
-
-        int[] answer;
-        // 경우를 나눠서 배열에 값을 저장하기
-        if(list.size() == 0) {
-            answer = new int[1];
-            answer[0] = -1;
-        } else {
-            answer = new int[list.size()];
-            for (int i = 0; i < list.size(); i++) {
-                answer[i] = list.get(i);
-            }
-        }
-
         return answer;
     }
 }
