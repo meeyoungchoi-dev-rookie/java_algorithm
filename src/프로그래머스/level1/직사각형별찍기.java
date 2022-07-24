8e90e600d36479822913bfc8becec66887cf6eac
meeyoungchoi <goodlife1359@gmail.com>
2022-07-24 19:12:22 +0900

    프로그래머스 Level1 직사각형 별 찍기

---
+++ src/프로그래머스/level1/직사각형별찍기.java
@@ -0,0 +1,18 @@
+package 프로그래머스.level1;
+
+import java.util.Scanner;
+
+public class 직사각형별찍기 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        for (int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
