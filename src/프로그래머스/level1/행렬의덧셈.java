a05cab7ffe3c607b2629508b3c54fea4d8e1f74f
meeyoungchoi <goodlife1359@gmail.com>
2022-07-27 22:59:05 +0900

    프로그래머스 Level1 행렬의 덧셈

---
+++ src/프로그래머스/level1/행렬의덧셈.java
@@ -0,0 +1,13 @@
+package 프로그래머스.level1;
+
+public class 행렬의덧셈 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] arr = new int[arr1.length][arr1[0].length];
+        for(int i=0; i<arr1.length; i++) {
+            for(int j=0; j<arr1[0].length; j++) {
+                arr[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return arr;
+    }
+}
